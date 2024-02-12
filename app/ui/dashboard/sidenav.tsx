7ad999aa5b46bb3d8723e376ea88925af3d3b5f5
43alsosc <43alsosc@stud.akademiet.no>
2024-02-12 11:09:52 +0100

    laget sidenavigasjonen til nettsiden

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -4,7 +4,7 @@ import Logo from "@/app/ui/logo";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2 dark:bg-black">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 dark:bg-black ">
       {/* Link for logoen, som sender deg til dashboard hjemmesiden når den blir klikket */}
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
