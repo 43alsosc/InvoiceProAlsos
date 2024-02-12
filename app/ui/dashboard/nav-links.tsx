03a2db6ca8321f319b597a29266d0bf50dab42db
43alsosc <43alsosc@stud.akademiet.no>
2024-02-12 11:10:27 +0100

    laget lenkene til sidenavigasjonen

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -39,7 +39,7 @@ export default function NavLinks() {
             href={link.href}
             // Dynamic classNames for styling based on the current pathname
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:bg-blue-600 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:bg-blue-600 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ",
               {
                 // Apply specific styles if it's the current page
                 "bg-sky-100 dark:text-blue-600 text-blue-500 dark:bg-sky-100":
