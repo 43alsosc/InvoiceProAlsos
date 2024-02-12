32129512b73a5b7e12718c51c1e657ccab502435
43alsosc <43alsosc@stud.akademiet.no>
2024-02-12 13:38:59 +0100

    fikset en bug med fargene

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -39,10 +39,9 @@ export default function NavLinks() {
             href={link.href}
             // Dynamic classNames for styling based on the current pathname
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium text-black hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ",
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                // Apply specific styles if it's the current page
-                "bg-blue-500  text-sky-100 ": pathname === link.href,
+                "bg-blue-200 text-black": pathname === link.href,
               }
             )}
           >
