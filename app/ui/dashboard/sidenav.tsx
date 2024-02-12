cb6f5dfc3e94ae27b661e194de071d63c0885b6d
43alsosc <43alsosc@stud.akademiet.no>
2024-02-12 13:17:00 +0100

    Fjernet dark mode intergrasjon

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,7 +5,7 @@ import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2 dark:bg-black ">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
       {/* Link for logoen, som sender deg til dashboard hjemmesiden når den blir klikket */}
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
@@ -16,12 +16,13 @@ export default function SideNav() {
           <Logo />
         </div>
       </Link>
+
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         {/* Viser tre bokser med linker til forskjellige sider som er angitt i "nav-links.tsx" */}
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 dark:bg-blue-600 md:block"></div>
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-100  md:block"></div>
 
-        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 text-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
           <PowerIcon className="w-6" />
           <div className="hidden md:block">Sign Out</div>
         </button>
