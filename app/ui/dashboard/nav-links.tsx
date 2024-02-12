c87e6728f1b96f8e0eb3852c3a7e8b3649130a86
43alsosc <43alsosc@stud.akademiet.no>
2024-02-12 10:57:45 +0100

    test

---
+++ app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+// Kart over lenker som skal vises i sidenavigasjonen.
+const links = [
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
+  {
+    name: "Invoices",
+    href: "/dashboard/invoices",
+    icon: DocumentDuplicateIcon,
+  },
+  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  // Hook to get the current pathname
+  const pathname = usePathname();
+
+  return (
+    <>
+      {/* Map through each link and create a nav item */}
+      {links.map((link) => {
+        // Get the icon component for the current link
+        const LinkIcon = link.icon;
+
+        return (
+          <Link
+            // Unique key for each link
+            key={link.name}
+            // URL the link points to
+            href={link.href}
+            // Dynamic classNames for styling based on the current pathname
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:bg-blue-600 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                // Apply specific styles if it's the current page
+                "bg-sky-100 dark:text-blue-600 text-blue-500 dark:bg-sky-100":
+                  pathname === link.href,
+              }
+            )}
+          >
+            {/* Link icon */}
+            <LinkIcon className="w-6" />
+            {/* Link name, hidden on smaller screens */}
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
---
+++ app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import Logo from "@/app/ui/logo";
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 dark:bg-black">
+      {/* Link for logoen, som sender deg til dashboard hjemmesiden når den blir klikket */}
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          {/* Logoen */}
+          <Logo />
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        {/* Viser tre bokser med linker til forskjellige sider som er angitt i "nav-links.tsx" */}
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 dark:bg-blue-600 md:block">
+          <p>Sign out</p>
+        </div>
+      </div>
+    </div>
+  );
+}
