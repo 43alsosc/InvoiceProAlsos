7eda2c1a1630022e0c5380988896d5ea30ce5623
43alsosc <43alsosc@stud.akademiet.no>
2024-02-13 09:58:11 +0100

    Lagt til invoice table

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,9 @@
+import InvoicesTable from "@/app/ui/invoices/invoice-table";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <InvoicesTable />
+    </main>
   );
 }
